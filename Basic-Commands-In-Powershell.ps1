47a77812e861dce1cbd112b166c8d26c7583ef8f
kristof de pauw <depauw.kristof@gmail.com>
2020-02-23 21:53:40 +0100

    added comments

--- Basic-Commands-In-Powershell.ps1
+++ Basic-Commands-In-Powershell.ps1
@@ -6,10 +6,11 @@ Get-Help Format-Table
 
 Get-Command -Module Microsoft.PowerShell.Management
 
+
 Get-Item .
 
 Get-Item *
-
+#get content of a file named demo.txt in the current directory, show only the first 8 lines
 Get-Content -Path .\demo.txt -TotalCount 8
 
 Start-Service -Name "eventlog"
@@ -25,6 +26,10 @@ Get-ExecutionPolicy -List
 Set-ExecutionPolicy -ExecutionPolicy AllSigned -Scope Process
 
 Copy-Item "C:\Users\DirName\demo.txt" -Destination "C:\Demo"
+cd \
+cd .\demo\
+dir
+
 
 Remove-Item C:\Users\DirName\*.*
 
