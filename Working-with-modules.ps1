4ffca4f4aa9218b7e71bf3dd32127dbf93b5f616
kristof de pauw <depauw.kristof@gmail.com>
2020-02-23 22:03:07 +0100

    Working with modules

---
+++ Working-with-modules.ps1
@@ -0,0 +1,10 @@
+#show all available modules
+Get-Module -ListAvailable
+#show installed modules
+Get-Module
+#show commands of a certain module
+Get-Command -Module (module name)
+
+Get-Help (command name)
+#if help is outdated
+Update-Help
